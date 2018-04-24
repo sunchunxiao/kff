5c967f4e5796b5cc97562ef8c9721035ca51ca6f
PC-20180106HYSJ\Administrator <1173597969@qq.com>
2018-04-24 21:02:21 +0800

    第一次提交

--- src/views/user/register.vue
+++ src/views/user/register.vue
@@ -19,7 +19,12 @@
       <img src="../../assets/image/logo.png" alt=""/>
     </div>
     <div class="page-part">
-      <mt-field label="手机号" placeholder="输入手机号" type="tel" v-model="phone"></mt-field>
+
+      <mt-field label="手机号" placeholder="输入手机号" type="tel" v-model="phone" id="telnum"  @blur.native.capture="handleCommentBlur"></mt-field>
+      <!--<div class="register-tel-error" id="telnum-error" style="opacity: 0;">-->
+        <!--<img src="../assets/sp-imgs/register-error.png" alt="" />-->
+        <!--<span>请输入正确的手机号</span>-->
+      <!--</div>-->
       <mt-field label="密码" placeholder="输入长度6-16位密码" type="password" v-model="password"></mt-field>
     </div>
     <div class="page-part">
@@ -53,7 +58,32 @@
         agree: false,
       }
     },
+    mounted(){
+
+    },
     methods: {
+      //注册验证
+      handleCommentBlur(){
+        var telInput = $("#telnum"); //获取输入框内容
+        var telnum = $("#telnum").val();　　 //    验证手机号码格式
+        console.log(this.phone)
+        var phone  = this.phone;
+          var myreg = /^1[34578]\d{9}$/;
+          console.log(phone)
+          console.log(myreg.test(phone));
+          if(!myreg.test(phone)) {
+            console.log(111)
+            alert("手机号格式不正确!")
+            // $("#telnum-error").css("opacity", "1");
+            return false;
+          } else {
+            console.log(222)
+            alert("注册成功")
+            // $("#telnum-error").css("opacity", "0")
+            return true;
+          }
+      },
+
       getCode() {
         console.log('获取验证码')
         const TIME_COUNT = 60;
@@ -71,7 +101,6 @@
           }, 1000)
         }
       },
-
       register() {
         this.$router.push('/main')
       },
