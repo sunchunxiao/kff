f85ce07da3cfd6516d87c0451973c854f0d9a6db
zcs <zhengchangshun928@126.com>
2018-05-02 09:27:57 +0800

    加密算法

--- src/components/project/introduction.vue
+++ src/components/project/introduction.vue
@@ -1,8 +1,22 @@
 <style lang="less">
+  .introduction {
 
+    .description {
+      white-space: normal;
+      height: 50px;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+  }
 </style>
 <template>
-  <div>简介</div>
+  <div class="introduction">
+    <h3>项目简介</h3>
+    <div class='description'>
+      NEO，原名小蚁，2014年正式立项，2015年6月于Github实时开源。成立以来，NEO团队亲历了区块链行业的高潮与低谷，数字货币市场的狂热与冷却，各国监管...
+      成立以来，NEO团队亲历了区块链行业的高潮与低谷，数字货币市场的狂热与冷却，各管的高潮与低谷
+    </div>
+  </div>
 </template>
 
 <script>
