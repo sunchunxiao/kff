5807b61dc2b96e511b30319698b1cad2f115ceaa
孙春晓 <15537791297@qq.com>
2018-05-29 11:30:16 +0800

    5.29关于我们和帮助中心

--- src/views/mine/about.vue
+++ src/views/mine/about.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="about">
     <HeaderBar
       :rightOptions="rightOptions"
       v-on:rightClickHandel="rightClickHandel"
@@ -9,18 +9,24 @@
       <!--<img src="../../assets/image/logo.png" alt="">-->
       <div>
         <div class="helpInfo">
-          <h3>区分</h3>
-          <p>全球首家以分布式测评为核心的区块链投资平台</p>
+          <div><img src="../../assets/mine/about/logo@2x.png" alt=""></div>
+          <h1>区分</h1>
+          <!--<p>全球首家以分布式测评为核心的区块链投资平台</p>-->
         </div>
         <div class="helpContent">
           <p>区分，全球首家以分布式评测为核心的区块链投资平台，当初针对区块链项目进行分布式评测，构建区块链行业独特的投资内容生态，形成包括项目评测，项目论坛，项目咨询，项目文章等高频率、高质量的UGC内容</p>
           <p>区分致力于成为全球区块链投资者首选的内容平台，打造丰富的区块链投资内容生态，构建用户层，内容层，社区层，投资层到数据层的生态闭环。</p>
           <p>区分旗下：区分大学、The Finder联盟、阳光项目计划。与区分产品相辅相成，共同构建了区分生态。</p>
           <h2>投资区块链，一定要 <span>区分</span> </h2>
-          <div>
-            <div>联系我们</div>
-            <div></div>
-          </div>
+
+        </div>
+      </div>
+      <div>
+        <div class="contact">联系我们</div>
+        <div class="contactIcon">
+          <img src="../../assets/mine/about/telegram@2x.png" alt="">
+          <img src="../../assets/mine/about/weixin@2x.png" alt="">
+          <img src="../../assets/mine/about/weibo@2x.png" alt="">
         </div>
       </div>
     </div>
@@ -28,17 +34,13 @@
 </template>
 
 <style>
-  .poster {
-    width: 90%;
-    margin: 0 auto;
+  .about{
+    height: 100%;
     background-color: #f2f2f2;
-    /*width:200px;*/
-    /*height: 200px;*/
-    /*position: absolute;*/
-    /*left:50%;*/
-    /*top:50%;*/
-    /*margin-left:-100px;*/
-    /*margin-top:-100px;*/
+  }
+  .poster {
+    /*width: 90%;*/
+    margin: 0 auto;
   }
   .mint-button-text{
     color:#3b88f6;
@@ -48,10 +50,38 @@
     padding: 2rem 0;
   }
   .helpContent{
+    background-color: #fff;
     padding: 2rem 1rem;
   }
   .helpContent p{
+    color: rgb(74,74,74);
+    font-size: 1.2rem;
     margin: 1rem 0;
+  }
+  .about h2{
+    font-family: PingFang-SC-bold;
+    text-align: center;
+    color: #28292d;
+    font-weight: bold;
+    font-size: 1.5rem;
+  }
+  .contact{
+    color: #4a4a4a;
+    font-size: 1.5rem;
+    text-align: center;
+    margin-top: 1rem;
+  }
+  .contactIcon{
+    /*float: left;*/
+    width: 50%;
+    margin:  0 auto;
+    display: flex;
+    justify-content: space-between;
+    margin-top: 1rem;
+  }
+  .contactIcon img{
+    width: 20%;
+    height: 20%;
   }
 </style>
 
--- src/views/mine/help.vue
+++ src/views/mine/help.vue
@@ -12,10 +12,36 @@
     <!-- tab-container -->
     <mt-tab-container v-model="selected">
       <mt-tab-container-item id="evaluate">
-        评测规则写在这里
+        <div class="evaluate-rule">
+          <h1>评测规则</h1>
+          <p class="margin-bottom mainB">为了让更多的人参与到针对区块链项目的分布式评测中，“区分”内提供了对区块链项目的多种评测方式，包括普通评测、专业评测（单纬度评测、完整评测、自定义评测）等多种模式。您可以根据需要进行选择。 根据不同用户对于一个区块链项目的评分，综合考虑内容贡献者和筛选人的区分指数，计算出当前项目的综合评分。</p>
+          <p class=" margin-bottom">注：区分的评测模型在不断进化和迭代中，初期版本V1.0经过区分团队的研究和实践，仍会有不足之处，后期迭代将由The Finder发现者联盟共同决定。 </p>
+          <div class="help-content">
+            <h3 class="help-title">1.评测方法</h3>
+            <div >1.简单评测 </div>
+            <div class=" mainB">当您对区块链项目有一定认知时，可对项目给出自己的评分，以及简单评价。</div>
+            <div>2.专业评测 </div>
+            <div  class=" mainB">如果您对区块链项目有深度的认知，你可以选择专业评测，贡献深度内容，并进行打分。</div>
+            <div>2.1 单纬度评测 </div>
+            <div  class=" mainB">“区分”提供了区块链项目的多个评测纬度，如果您对某一方面（例如项目团队）有深入研究，选择“单纬度评测”，可以给出自己相应的专业分析及评分。</div>
+            <div>2.2 完整评测 </div>
+            <div  class=" mainB">如果您对当前区块链项目有着完整的认知和观点时，可以选择“完整评测”，对“区分”提供的几个纬度给出自己的评分和每个纬度的详细分析。“区分”会根据您对各纬度的评分和权重，计算出综合评分。综合评分 = SUM（纬度评分*当前纬度权重）</div>
+            <div>2.3 自定义评测</div>
+            <div  class=" mainB">如果您或您的团队，有着专业的区块链项目评测模型，可以在“区分”上新建自己的评测模型（包括纬度名称、权重、分数，权重之和为1）。对当前项目进行评分，给出客观公正的评测报告。</div>
+            <p  class=" mainB">“区分”会根据您对各纬度的评分和权重，计算出综合评分。 综合评分 = SUM（纬度评分*当前纬度权重）</p>
+            <h3 class="help-title">2.项目评分</h3>
+            <p class=" mainB">对于某个区块链项目，除了单份评测报告给予区块链项目的评分，“区分”还将综合考虑当前区块链项目评测的所有参与者，对区块链项目给予一个综合评分。当参与项目评测的人数到达一定数量时，系统会自动对区块链项目进行整体评分，计算公式</p>
+            <div>如下:</div>
+            <img class="helpEvaluation" src="../../assets/mine/help/computed.png" alt="">
+            <div class="summary">
+              对项目进行整体评分时综合考虑内容贡献者和筛选者的区分指数，最大限度地保证评测的专业性和公正性。项目整体评分可以直观的反映出项目等级，给予投资者相应的投资参考。
+            </div>
+          </div>
+        </div>
       </mt-tab-container-item>
       <mt-tab-container-item id="reward">
-        激励机制写在这里
+        <h1>激励机制</h1>
+        <p>为了“区分”生态的健康发展，我们设计了FIND的消耗与获取闭环。初期为了区分生态的健康发展，“区分”对于注册用户给予了FIND奖励。同时鼓励优质内容，区分对贡献优质内容、提供有价值信息的用户给予FIND奖励。</p>
       </mt-tab-container-item>
       <mt-tab-container-item id="description">
         项目说明写在这里
@@ -26,8 +52,31 @@
 </template>
 
 <style lang="less">
+  .evaluate-rule h1{
+    font-size: 15px;
+    margin: 1rem 0;
+  }
   .mint-tab-container-item {
    padding: 10px 20px
+  }
+  .helpEvaluation{
+    width: 100%;
+  }
+  .margin-bottom{
+    font-size: 1.1rem;
+    color: rgb(74,74,74);
+  }
+  .help-title{
+    color: mediumvioletred;
+  }
+  .help-content{
+    font-size: 1rem;
+  }
+  .mainB{
+    margin-bottom: 1rem;
+  }
+  .summary{
+    font-size: 1.2rem;
   }
 </style>
 
