461049257cd16d3797c07f735cad37ec62309e1e
孙春晓 <15537791297@qq.com>
2018-06-27 20:47:56 +0800

    6.27 分享5 link

--- src/assets/js/wxshare.js
+++ src/assets/js/wxshare.js
@@ -18,15 +18,14 @@ $.ajax({
   },
   dataType:"json",
   success(data){
-    console.log(111)
     console.log(data)
     wx.config({
-          debug: false, // 开启调试模式,开发时可以开启
+          debug: false,
           appId: "wxd207589d26688a4a",   // 必填，公众号的唯一标识   由接口返回
           timestamp: data.timestamp, // 必填，生成签名的时间戳 由接口返回
           nonceStr: data.nonceStr,    // 必填，生成签名的随机串 由接口返回
           signature: data.signature,   // 必填，签名 由接口返回
-          jsApiList: ['onMenuShareAppMessage', 'onMenuShareTimeline'] // 此处填你所用到的方法
+          jsApiList: ['checkJsApi','onMenuShareAppMessage', 'onMenuShareTimeline'] // 此处填你所用到的方法
         });
   }
 })
@@ -35,20 +34,20 @@ wx.ready(function () {
   wx.checkJsApi({
     jsApiList: [
       'getNetworkType',
-      'previewImage'
+      'previewImage',
+      'onMenuShareAppMessage'
     ],
     success: function (res) {
-      alert(1)
+      alert(res)
     }
   });
   var title = '表头';
   var desc = '线下商场与线上商城，同步更新，同享优惠。现在注册，立享150元优惠券，3月更多活动，精彩不断。';
   var imgUrl = 'http://p3.pstatp.com/large/pgc-image/152887919766950e17bb4bc'
-
   wx.onMenuShareAppMessage({ // 分享给朋友
     title: title,       // 分享标题
     desc: desc,   // 分享描述
-    link: url,       // 分享链接 默认以当前链接
+    link: "https://m.qufen.top",       // 分享链接 默认以当前链接
     imgUrl: imgUrl,// 分享图标
     // 用户确认分享后执行的回调函数
     success: function () {
@@ -68,7 +67,7 @@ wx.ready(function () {
 
   wx.onMenuShareTimeline({
     title: title,
-    link: url,
+    link: "https://m.qufen.top",
     imgUrl: imgUrl,
     trigger: function (res) {
       // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
@@ -79,6 +78,10 @@ wx.ready(function () {
     },
     fail: function (res) {
     }
+  });
+  wx.error(function(res){
+    alert("error")
+    // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
   });
 
 })
