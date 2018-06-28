fd4a68e70ebba7370efd3f72afe8ca38aeeb185f
孙春晓 <15537791297@qq.com>
2018-06-28 09:28:10 +0800

    6.27 分享隐藏弹框

--- src/assets/js/wxshare.js
+++ src/assets/js/wxshare.js
@@ -38,7 +38,7 @@ wx.ready(function () {
       'onMenuShareAppMessage'
     ],
     success: function (res) {
-      console.log(res)
+      // console.log(res)
     }
   });
   var title = '表头';
@@ -80,7 +80,7 @@ wx.ready(function () {
     }
   });
   wx.error(function(res){
-    alert("error")
+    console.log("error")
     // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
   });
 
