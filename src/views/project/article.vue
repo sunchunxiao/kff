e1a927c497b9ea2b27e92d640bf5e54157015cc1
孙春晓 <15537791297@qq.com>
2018-06-29 14:15:01 +0800

    6.29 二次分享当前路径2

--- src/views/project/article.vue
+++ src/views/project/article.vue
@@ -129,7 +129,7 @@
 		},
 		updated() {
 		  console.log(this.imgUrl)
-      console.log(window.location.href.split("&")[0])
+      console.log(window.location.href)
       if(this.imgUrl.length==0){
         this.imgUrlwx = 'https://pic.qufen.top/posts20180628204925934317'
       }else{
